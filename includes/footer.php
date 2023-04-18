b81b0ae28ba6fd03cf135714e1d27862ca21f6d7
hh-api <131185043+hh-api@users.noreply.github.com>
2023-04-19 02:31:33 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
 <div id="footer"><footer id="colophon" class="site-footer" itemscope="itemscope" itemtype="http://schema.org/WPFooter" role="contentinfo"><div class="footercopyright"><div class="copyright marx"><div class="footer-logo">
+<a target="_blank" href="https://t.me/+fRK6LPYsUl1jYzU1"><img width="50px" src="https://lh3.googleusercontent.com/-VGdNCwWEI6Y/ZD7vPphlzeI/AAAAAAAADBA/Y40IlX_arRQYleYQ_JW3BzFk2M-f__fCACNcBGAsYHQ/s0/tele.png"/></a>    
+<br/>
 <a href="/"><img width="300px" alt="Xem Hoạt Hình, Anime Tại ZuiGhe.Net" src="https://media.discordapp.net/attachments/924155580124385280/1072038465899859999/zuighe4.png"></a>
 </div>
 <div class="txt"> <span>Copyright © 2023 All Rights Reserved</span></div></div></div></footer></div> 
