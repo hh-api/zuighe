8635b722041bae8672018c6a95de65042ba2aa90
hh-api <131185043+hh-api@users.noreply.github.com>
2023-04-18 23:48:09 +0700

    Create chat.php

---
+++ includes/chat.php
@@ -0,0 +1,7 @@
+<div id="sidebar" style="width:29%">
+<div class="section">
+<div class="releases" style="text-align:center"><h3> Khu Vực Chém Gió</h3></div>
+<div class='serieslist pop'>
+<iframe src="https://www5.cbox.ws/box/?boxid=943008&boxtag=WhJ8ti" width="100%" height="450" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>
+</div></div>
+</div>
